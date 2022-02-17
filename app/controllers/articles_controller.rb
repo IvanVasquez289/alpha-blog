40bad61f58d830735cc82d34b70caf47e99e52dd
Ivan Vasquez <ivanjv1234@gmail.com>
2022-02-17 17:36:47 -0600

    feat: add resources

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,59 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all 
   end
+
+  def new
+    @article = Article.new  
+  end
+
+  def edit
+    # edit y update confunden algo, pero edit es de tipo get, por lo que es una vista, aqui solo renderizamos
+    # segun el id que de el params, y update es tipo PUT,asi que una vez modificado un article,debe hacer render
+    @article = Article.find(params[:id])
+  end
+  
+
+  def create
+    # este comando renderiza en el navegador lo que cremos, pero mejor lo guardamos en una var pa la BD
+    #render plain: params[:article]
+    # @article = Article.new(params[:article]) tampoco es asi
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect aqui ya se ve como cuando en la consola creamos un articulo, con los demas fils vacios
+    #@article.save
+
+    # una vez hecho te debe redirigir al show action para mostrar el articulo que acabas de crear
+    # nos vamos a routes --expanded y vemos que el prefix de show es article, entonces ese es mi path article_path
+    #como la ruta es /articles/:id , levamos a pasar entre parentesis la instancia article y rails solito la va a extraer
+
+    # redirect_to article_path(@article) esto se puede simplificar asi
+    #redirect_to @article
+
+    if @article.save
+      flash[:notice] = "El articulo fue creado correctamente"
+      redirect_to @article
+    else
+      render 'new'
+    end
+
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "El articulo fue actualizado correctamente"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+   
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+  
+  
 end
